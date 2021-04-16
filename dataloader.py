af3e8b401039d5ec418e182683dd5954a6a45cef
gavinstjohn <gavinastjohn@gmail.com>
2021-04-16 13:34:51 -0400

    added quadrant cutup code but the case statement syntax is broken, not sure why

--- dataloader.py
+++ dataloader.py
@@ -42,6 +42,7 @@ class PlantStressDataset(Dataset):
         #       (0: top left, 1: top right, 2: bottom left, 3: bottom right)
         sample_id = idx[0]
         exp_id = idx[1]
+        print(exp_id)
         if torch.is_tensor(sample_id):
             sample_id = sample_id.tolist()
 
@@ -50,7 +51,7 @@ class PlantStressDataset(Dataset):
         
         img_name_g = os.path.join(self.img_dir, img_names[0])
         image_g = io.imread(img_name_g)
-        print(image_g)
+        print(type(image_g))
 
         img_name_b = os.path.join(self.img_dir, img_names[1])
         image_b = io.imread(img_name_b)
@@ -63,6 +64,40 @@ class PlantStressDataset(Dataset):
 
         img_names = [img_name_g, img_name_b, img_name_nir, img_name_r]
 
+
+        # cut up images on quadrant basis
+        # i feel like there's a more concise way to do this
+        # also might be worth changing the cases to match the quadrants of an x-y plane 
+        # ie I is top right, II top left, III bottom left, IV bottom right
+        h,w = image_g.shape # grab size of img, could use any of them 
+        height_cutoff = h//2 # get the middles of height and width
+        width_cutoff = w//2
+        print(exp_id)
+        print(type(exp_id))
+        # based on which experiment id is passed in return a different quadrant
+        match exp_id: 
+            case 0: # top left
+                image_g = image_g[:height_cutoff, :width_cutoff]
+                image_b = image_b[:height_cutoff, :width_cutoff]
+                image_nir = image_nir[:height_cutoff, :width_cutoff]
+                image_r = image_r[:height_cutoff, :width_cutoff]
+            case 1: # top right
+                image_g = image_g[:height_cutoff, width_cutoff:]
+                image_b = image_b[:height_cutoff, width_cutoff:]
+                image_nir = image_nir[:height_cutoff, width_cutoff:]
+                image_r = image_r[:height_cutoff, width_cutoff:]
+            case 2: # bottom left
+                image_g = image_g[height_cutoff:, :width_cutoff]
+                image_b = image_b[height_cutoff:, :width_cutoff]
+                image_nir = image_nir[height_cutoff:, :width_cutoff]
+                image_r = image_r[height_cutoff:, :width_cutoff]
+            case 3: # bottom right
+                image_g = image_g[height_cutoff:, width_cutoff:]
+                image_b = image_b[height_cutoff:, width_cutoff:]
+                image_nir = image_nir[height_cutoff:, width_cutoff:]
+                image_r = image_r[height_cutoff:, width_cutoff:]
+
+
         # pul, 'img_channel': img_channel}
         # pulls in the image
         image = np.array([image_g, image_b, image_nir, image_r])
@@ -71,14 +106,10 @@ class PlantStressDataset(Dataset):
         stress_time = self.labels.iloc[sample_id,2]
         img_channel = literal_eval(self.labels.iloc[sample_id,3])
 
-
-
         # constructs dict of image and labels
         sample = {'image': image, 'capture_time': capture_time, 'stress_time': stress_time, 'img_channel': img_channel}
 
-        # sample = np.array([TL,TR,BL,BR]) 
-
-        if self.transform
+        if self.transform:
             sample = self.transform(sample)
 
         return sample
@@ -131,6 +162,7 @@ def main():
             img_dir='/blue/eel6935/gavinstjohn/plant/plant-imaging/images/experiment_20210210_1/')
 
     sample_101 = plant_dataset[101,2] # pass a tuple in
+    print(sample_101["image"][0])
 
     """t_plant_dataset = PlantStressDataset(
             csv_file='/blue/eel6935/gavinstjohn/plant/plant-imaging/images/labels_20210210_01.csv',
