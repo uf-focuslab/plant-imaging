aa23bae02110f4713b02f6c7651113fbbea81bf1
gavinstjohn <gavinastjohn@gmail.com>
2021-06-28 12:59:48 -0400

    reconfig dataloader to play nicely with convlstm

--- testing.py
+++ testing.py
@@ -16,14 +16,18 @@ def main():
     dataset = PlantStressDataset(
                 csv_file = root + 'images/labels_20210210_01.csv',
                 img_dir = root + 'images/experiment_20210210_1/',
+                seq_length = 10, 
+                quadrant = 0,
                 transform = Mask(8))
+                
+    print(dataset[torch.tensor([0,1,2,3])])
 
     dataloader = torch.utils.data.DataLoader(dataset=dataset, 
             batch_size = 5, 
             shuffle = True)
 
 
-    print(dataloader[0])
+    #print(dataloader[0])
 
 if __name__ == "__main__":
     main()
